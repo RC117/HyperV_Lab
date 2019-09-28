9f6e42b44da64f3a040d6dc76e7f22b11d337918
RC <615568+RC117@users.noreply.github.com>
2019-09-28 15:20:15 +0100

    Some refactoring

--- test.ps1
+++ test.ps1
@@ -2,11 +2,37 @@
 # create a vm connecting to that new diff disk
 
 # Create diff disk
-New-VHD -Path D:\VMs\Test1Diff.vhdx -ParentPath 'D:\VMs\Base\Virtual Hard Disks\Base.vhdx' -Differencing
+try {
+    New-VHD -Path D:\VMs\Test1Diff.vhdx -ParentPath 'D:\VMs\Base\Virtual Hard Disks\Base.vhdx' -Differencing
+}
+Catch {
+    Throw "The Hard disk could not be created"
+}
 
 # create vm using new diff disk
+try {
+    New-VM -Name "Test1" -Generation 2 -MemoryStartupBytes 3072MB -VHDPath 'D:\VMs\Test1Diff.vhdx' -SwitchName Lan
+    
+}
+Catch {
+    Throw "The VM could not be created"
+}
 
-New-VM -Name "Test1" -Generation 2 -MemoryStartupBytes 3072MB -VHDPath 'D:\VMs\Test1Diff.vhdx' -SwitchName Internet
-Get-VM Test1 | Add-VMNetworkAdapter -SwitchName Lan
-Start-VM -Name Test1
+# Start VM
+try {
+    Start-VM -Name Test1
+    Enable-VMIntegrationService -VMName Test1 -Name "Guest Service Interface"
+    # Invoke-Command -VMName Test1 -ScriptBlock { (Get-NetAdapter).InterfaceAlias } -Credential $cred
+    # Get-VM Test1 | Add-VMNetworkAdapter -SwitchName Internet        
+}
+catch {
+    Throw "Could not start VM"
+}
 
+try {
+    # Invoke-Command -VMName Test1 -ScriptBlock { (Get-NetAdapter).InterfaceAlias } -Credential $cred
+    # New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.10 -PrefixLength 24 -DefaultGateway 192.168.1.1
+}
+catch {
+    
+}
