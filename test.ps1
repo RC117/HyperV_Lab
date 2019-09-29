5e63f1ce8486d45309dfc77792f0598c9f3ca1c1
RC <615568+RC117@users.noreply.github.com>
2019-09-29 13:34:31 +0100

    Remove VM and VHDs

--- test.ps1
+++ test.ps1
@@ -1,5 +1,21 @@
+# TODO
 # create a diff disk for each vm
 # create a vm connecting to that new diff disk
+
+# Remove VM and it's VHDs
+try {
+    $VM = Get-VM -VMName Test1 -ErrorAction SilentlyContinue
+
+    if ($Null -ne $VM) {
+        $vmvhds = $VM | Select-Object VMId | Get-VHD
+        Stop-VM -VMName Test1 -Force
+        Remove-VM -VMName Test1 -Force
+        $vmvhds | ForEach-Object { Remove-Item -Path $_.ParentPath -Recurse -Force -Confirm:$false }
+    }
+}
+catch {
+    Throw "The VM does not exist"
+}
 
 # Create diff disk
 try {
@@ -29,10 +45,15 @@ catch {
     Throw "Could not start VM"
 }
 
-try {
-    # Invoke-Command -VMName Test1 -ScriptBlock { (Get-NetAdapter).InterfaceAlias } -Credential $cred
-    # New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.10 -PrefixLength 24 -DefaultGateway 192.168.1.1
-}
-catch {
+
+
+# TODO: get network adapter and then change it's ip
+# TODO: Then add "Internet" as new network adapter
+
+# try {
+#     # Invoke-Command -VMName Test1 -ScriptBlock { (Get-NetAdapter).InterfaceAlias } -Credential $cred
+#     # New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.1.10 -PrefixLength 24 -DefaultGateway 192.168.1.1
+# }
+# catch {
     
-}
+# }
