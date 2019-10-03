857f3301202506906cc12a7a0c7e1f1badd53c86
RC <615568+RC117@users.noreply.github.com>
2019-10-03 19:24:41 +0100

    Some refactoring

--- test2.ps1
+++ test2.ps1
@@ -1,10 +1,12 @@
 . .\VmModule.ps1
 
+$path = "D:\VMs"
+$parentPath = "D:\VMs\Base\Virtual Hard Disks\Base.vhdx"
 Remove-VMAndVHD -VmName Test1
 Remove-VMAndVHD -VmName Test2
 Remove-VMAndVHD -VmName Test3
-Add-DiffVm -VmName Test1 -Path "D:\VMs" -ParentPath "D:\VMs\Base\Virtual Hard Disks\Base.vhdx" -SwitchName Lan
-Add-DiffVm -VmName Test2 -Path "D:\VMs" -ParentPath "D:\VMs\Base\Virtual Hard Disks\Base.vhdx" -SwitchName Lan
-Add-DiffVm -VmName Test3 -Path "D:\VMs" -ParentPath "D:\VMs\Base\Virtual Hard Disks\Base.vhdx" -SwitchName Lan
+Add-DiffVm -VmName Test1 -Path $path -ParentPath $parentPath -SwitchName Lan
+Add-DiffVm -VmName Test2 -Path $path -ParentPath $parentPath -SwitchName Lan
+Add-DiffVm -VmName Test3 -Path $path -ParentPath $parentPath -SwitchName Lan
 
 # TODO: change ip on new vm, add Internet switch,turn on vm
